96922711fd028a73b786554e97974dd61f7ea04a
SEUNGWOO LEE <seungwoo.lee.518@proton.me>
2023-04-16 15:30:23 +0900

    chore(.config/home-manager): add home manager configuration

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,87 @@
+{ config, pkgs, ... }:
+
+let
+  pkgsUnstable = import <nixpkgs-unstable> {};
+in
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "seungwoo";
+  home.homeDirectory = "/home/seungwoo";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "22.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    pkgs.git
+		pkgs.gnumake
+    pkgsUnstable.go
+    pkgsUnstable.rustup
+    pkgsUnstable.kubectl
+    pkgsUnstable.neovim
+    pkgsUnstable.nodejs-18_x
+    pkgsUnstable.nil
+		pkgsUnstable.jq
+		pkgsUnstable.zed
+		pkgsUnstable.gcc
+		pkgsUnstable.podman
+		pkgsUnstable.nettools
+		pkgsUnstable.protobuf
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/seungwoo/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "$EDITOR";
+		GOPATH = "/home/seungwoo/go";
+		PATH="$HOME/.npm-global/bin:$GOPATH/bin:$PATH";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
