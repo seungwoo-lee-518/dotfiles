a67fd5558f8e5fab2cb12cba74f95d31ef5482b9
SEUNGWOO LEE <seungwoo.lee.518@proton.me>
2023-12-10 12:15:38 +0900

    chore(etc/nixos/configuration.nix): add swaywm configuration

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -1,42 +1,55 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  dbus-sway-environment = pkgs.writeTextFile {
+    name = "dbus-sway-environment";
+    destination = "/bin/dbus-sway-environment";
+    executable = true;
+
+    text = ''
+      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
+      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr      
+    '';
+  };
+  configure-gtk = pkgs.writeTextFile {
+    name = "configure-gtk";
+    destination = "/bin/configure-gtk";
+    executable = true;
+    text = let
+      schema = pkgs.gsettings-desktop-schemas;
+      datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+    in ''
+      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+      gnome_schema=org.gnome.desktop.interface
+      gsettings set $gnome_schema gtk-theme 'Dracula'
+    '';
+  };
+in
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
+      ./disko-config.nix
     ];
 
-  # Bootloader.
+  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # Cleans up "/tmp" on Boot
-  boot.tmp.cleanOnBoot = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  # Additional Configuration for Captive Portal
-  networking.extraHosts = ''
-    211.39.134.20 first.wifi.olleh.com
-  '';
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Asia/Seoul";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.inputMethod.enabled = "kime";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "ko_KR.UTF-8";
     LC_IDENTIFICATION = "ko_KR.UTF-8";
@@ -49,128 +62,16 @@
     LC_TIME = "ko_KR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "kr";
-    xkbVariant = "kr104";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  hardware.bluetooth.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
-  
-  # Intel GPU 가속을 할 수 있도록 코덱 설정 추가  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
-
-  # OpenGL 을 사용하고, 해당 설정에 Intel GPU Driver
-  # 을 추가하였습니다.
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver 
-      vaapiVdpau
-      libvdpau-va-gl
-    ];
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.seungwoo = {
     isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
-    description = "seungwoo";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
       firefox
-      slack
-      kate
-      helix
-      zellij
-      git
-      nil
-      dogdns
-      jq
-      gojq
-      gcc
-      stdenv.cc.cc.lib
-      zlib
-      zlib-ng
-      viddy
-      unzip
-      ffmpeg
-      vlc
-      kustomize
-      gitui
-      okteta
-      docker-compose
     ];
   };
-  
-  # CJK 폰트를 사용하지 않으면  # 한국어가 자글자글 거리기 때문에  # 21 세기를 살아가는 사람에게 해롭습니다.
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    # NerdFont 가 필요한 경우가 있어서    # 폰트를 추가하였습니다.
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
-  ];
 
-  
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "seungwoo";
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    intel-gpu-tools
-    curl
-    neofetch
-    imagemagick
-    fwupd
-    btrfs-progs
-    wireguard-tools
-    kernelshark
-    trace-cmd
-    libnbd
-  ];
-
-  # Enable Network Block Device support
-  programs.nbd.enable = true;
-
-  services.fwupd.enable = true;
-
+  # Enable zsh
   environment.shells = with pkgs; [ zsh ];
   environment.variables = {
     EDITOR="hx";
@@ -185,35 +86,84 @@
     theme = "robbyrussell";
   };
 
-  # Enable Docker  
-  virtualisation = {
-    docker = {
-      enable = true;
-    };
+  environment.systemPackages = with pkgs; [
+    helix
+    curl
+    wget
+    git
+    neofetch
+    btop
+    htop
+
+    intel-gpu-tools
+
+    # alacritty
+    # kitty
+    # foot
+    libsForQt5.konsole
+    dbus-sway-environment
+    configure-gtk
+    wayland
+    xdg-utils
+    glib
+    dracula-theme
+    gnome3.adwaita-icon-theme
+    swaylock
+    swayidle
+    grim
+    slurp
+    wl-clipboard
+    bemenu
+    mako
+    wdisplays
+  ];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
-  # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 
-  programs.ssh.startAgent = true;
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+  ];
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
 
+  services.dbus.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  # Enable Flakes
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  
+  system.stateVersion = "23.11"; # Did you read the comment?
 
 }
+
